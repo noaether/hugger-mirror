0aa192f37e24a848c27c02d418d74bd0c6579cc1
noae <67566122+noaether@users.noreply.github.com>
2024-04-26 21:52:34 -0400

    getting things started !!

---
+++ src/main/java/ca/noae/hugger/Hugger.java
@@ -0,0 +1,17 @@
+package ca.noae.hugger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hugger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
