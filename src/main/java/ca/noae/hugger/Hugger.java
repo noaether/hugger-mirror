8621ecae3124288f3ebbff963c61ecc04640745c
noae <67566122+noaether@users.noreply.github.com>
2024-05-12 19:54:16 -0400

    command executors

--- src/main/java/ca/noae/hugger/Hugger.java
+++ src/main/java/ca/noae/hugger/Hugger.java
@@ -4,9 +4,11 @@ import ca.noae.hugger.Commands.Interactions.Positive.*;
 import ca.noae.hugger.Commands.Interactions.Neutral.*;
 import ca.noae.hugger.Commands.Interactions.Negative.*;
 import ca.noae.hugger.Commands.Interactions.Punishments.*;
+import ca.noae.hugger.Commands.Interactions.Rewards.*;
 
 import ca.noae.hugger.API.Config;
 import ca.noae.hugger.Listeners.PlayerInteract;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -48,23 +50,59 @@ public final class Hugger extends JavaPlugin {
 
     public Set<Map.Entry<String, CommandExecutor>> registerNegativeCommands() {
         // initialize hashmap
-        HashMap<String, CommandExecutor> commands = new HashMap<>();
-            commands.put("burn", new Burn());
-            commands.put("hurt", new Hurt());
-            commands.put("shock", new Shock());
-            commands.put("slap", new Slap());
-            commands.put("spank", new Spank());
-            commands.put("whip", new Whip());
+        HashMap<String, CommandExecutor> negativeCommands = new HashMap<>();
+            negativeCommands.put("burn", new Burn());
+            negativeCommands.put("hurt", new Hurt());
+            negativeCommands.put("shock", new Shock());
+            negativeCommands.put("slap", new Slap());
+            negativeCommands.put("spank", new Spank());
+            negativeCommands.put("whip", new Whip());
 
-        return initializeEntries(commands);
+        return initializeEntries(negativeCommands);
     }
 
     public Set<Map.Entry<String, CommandExecutor>> registerNeutralCommands() {
         // initialize hashmap
-        HashMap<String, CommandExecutor> commands = new HashMap<>();
-            commands.put("poke", new Poke());
+        HashMap<String, CommandExecutor> neutralCommands = new HashMap<>();
+            neutralCommands.put("nudge", new Nudge());
+            neutralCommands.put("poke", new Poke());
+            neutralCommands.put("tickle", new Tickle());
 
-        return initializeEntries(commands);
+        return initializeEntries(neutralCommands);
+    }
+
+    public Set<Map.Entry<String, CommandExecutor>> registerPositiveCommands() {
+        // initialize hashmap
+        HashMap<String, CommandExecutor> positiveCommands = new HashMap<>();
+            positiveCommands.put("cuddle", new Cuddle());
+            positiveCommands.put("hug", new Hug());
+            positiveCommands.put("kiss", new Kiss());
+            positiveCommands.put("love", new Love());
+            positiveCommands.put("pet", new Pet());
+
+        return initializeEntries(positiveCommands);
+    }
+
+    public Set<Map.Entry<String, CommandExecutor>> registerPunishmentCommands() {
+        // initialize hashmap
+        HashMap<String, CommandExecutor> punishmentCommands = new HashMap<>();
+            punishmentCommands.put("chain", new Chain());
+            punishmentCommands.put("crush", new Crush());
+            punishmentCommands.put("cuff", new Cuff());
+            punishmentCommands.put("leash", new Leash());
+            punishmentCommands.put("muzzle", new Muzzle());
+
+        return initializeEntries(punishmentCommands);
+    }
+
+    public Set<Map.Entry<String, CommandExecutor>> registerRewardCommands() {
+        // initialize hashmap
+        HashMap<String, CommandExecutor> rewardCommands = new HashMap<>();
+            rewardCommands.put("feed", new Feed());
+            rewardCommands.put("praise", new Praise());
+            rewardCommands.put("treat", new Treat());
+
+        return initializeEntries(rewardCommands);
     }
 
 
