ae371bc8afce08d4188238a6164f18215fdc877b
noae <67566122+noaether@users.noreply.github.com>
2024-05-12 19:53:56 -0400

    rewards

---
+++ src/main/java/ca/noae/hugger/Commands/Interactions/Rewards/Treat.java
@@ -0,0 +1,12 @@
+package ca.noae.hugger.Commands.Interactions.Rewards;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Treat implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        return false;
+    }
+}
